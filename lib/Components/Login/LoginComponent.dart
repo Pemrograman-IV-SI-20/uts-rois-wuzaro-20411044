cea7456df93d95b8b5816b2eb29c12e69665e6f2
karvin06 <karvin.vin06@gmail.com>
2022-11-16 20:57:18 +0700

    UTS karvin

--- lib/Components/Login/LoginComponent.dart
+++ lib/Components/Login/LoginComponent.dart
@@ -1,48 +1,49 @@
+import 'dart:html';
+
 import 'package:flutter/cupertino.dart';
 import 'package:simple_shadow/simple_shadow.dart';
 import 'package:toko_gitar_flutter/Components/Login/LoginForm.dart';
+import 'package:toko_gitar_flutter/Utils/constants.dart';
 import 'package:toko_gitar_flutter/size_config.dart';
-import 'package:toko_gitar_flutter/utils/constants.dart';
 
-class LoginComponent extends StatefulWidget{
-_LoginComponent createState() => _LoginComponent();
+class LoginComponent  extends StatefulWidget{
+ @override
+ _LoginComponent createState()=> _LoginComponent();
 }
-class _LoginComponent extends State<LoginComponent> {
+class _LoginComponent extends State<LoginComponent>{
   @override
-Widget build(BuildContext context) {
+Widget build(BuildContext context){
   return SafeArea(
     child: SizedBox(
       width: double.infinity,
       child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: getProportionateScreenHeight(20)),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(
-                height: SizeConfig.screenHeight * 0.04,
-                ),
+        padding: EdgeInsets.symmetric(
+          horizontal: getProportionateScreenHeight(20)),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
                 SizedBox(
-                  height: SizeConfig.screenHeight * 0.04,
+                  height: SizeConfig.screenHeight *0.04,
+                ),
+                 SizedBox(
+                  height: SizeConfig.screenHeight *0.04,
                 ),
                 SimpleShadow(
-                  child: Image.asset("assets/imeges/guitar.png",
-                  height: 150,
-                  width: 202,
-                  ), 
-                  opacity: 0.5,
-                  color: kSecondaryColor,
-                  offset: Offset(5, 5),
-                  sigma: 2,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(left: 10),
+                  child: Image.asset(
+                    "assets/imeges/guitar.png",
+                    height: 100,width: 202,
+                    ),
+                    opacity: 0.5,
+                    color: kSecondaryColor,
+                    offset:  Offset(5, 5),
+                    sigma:  2,
+                    ),
+                    Padding(padding: EdgeInsets.only(left: 10),
                     child: Row(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(
-                          "Login !",
-                          style: mTitleStyle,
-
+                        Text("Login !",
+                        style: mTitleStyle,
                         )
                       ],
                     )),
@@ -50,10 +51,10 @@ Widget build(BuildContext context) {
                       height: 20,
                       ),
                       SignInform()
-                     ],
-        ),
+              ],
+              ),
+          ),
       ),
-    ),
   ),
   );
 }
